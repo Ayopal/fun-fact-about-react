e16accc1e7d7948aba36ab44cc2f0ab802c3df47
Ayopal <Ayopaul19@gmail.com>
2022-08-25 04:11:33 +0100

    my first react app

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+	return (
+		<footer>
+			<small>First react app developed by Ayoola Paul &copy; 2022</small>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from "react";
+
+function NavBar() {
+	return (
+		<header>
+			<nav className='navbar'>
+				<img src='/image/react-logo.png' alt='' />
+				<h3>ReactFacts</h3>
+                <h4>React Course-Project 1</h4>
+			</nav>
+		</header>
+	);
+}
+
+export default NavBar;
